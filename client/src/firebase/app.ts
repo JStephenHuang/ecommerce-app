ee906e117bf9bd83a704cf61a7eefbbaa269120a
Benny Huang <bennyhuang@Bennys-MacBook-Pro.local>
2022-09-19 19:49:10 -0400

    added login ui

---
+++ client/src/firebase/app.ts
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkYyMUACuyEh-odBAjIrAdNTv8QdFs1Yc",
+  authDomain: "ecommerce-app-76de8.firebaseapp.com",
+  projectId: "ecommerce-app-76de8",
+  storageBucket: "ecommerce-app-76de8.appspot.com",
+  messagingSenderId: "158161092140",
+  appId: "1:158161092140:web:19d56e3c5aabb5932c1b89",
+  measurementId: "G-51P6TF7QEY",
+};
+
+export const app = initializeApp(firebaseConfig);
--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,5 +1,79 @@
 const LoginPage = () => {
-  return <div>Login Page</div>;
+  return (
+    <div className="flex h-screen">
+      <div className="bg-[#111111] h-3/4 w-1/2 m-auto rounded-[25px] shadow-lg">
+        <div className="bg-[#ffffff] flex h-full w-1/2 rounded-[25px]">
+          <div className="m-auto w-1/2">
+            <div>
+              <p className="text-[32px] text-[#111111]">Welcome back</p>
+              <p className="text-[16px] text-[#111111]">
+                Please enter your details.
+              </p>
+            </div>
+
+            <br />
+            <br />
+            <div>
+              <p className="text-[16px] text-[#111111]">Email</p>
+              <input
+                type="email"
+                className="w-full h-10 border-2 border-[#dcdcdc] rounded-lg p-2"
+              />
+            </div>
+            <br />
+            <div>
+              <p className="text-[16px] text-[#111111]">Password</p>
+              <input
+                type="password"
+                className="w-full h-10 border-2 border-[#dcdcdc] rounded-lg p-2"
+              />
+            </div>
+            <br />
+            <br />
+            <div>
+              <button className="bg-[#111111] text-[#ffffff] w-full h-10 rounded-lg">
+                Sign in
+              </button>
+            </div>
+            <br />
+            {/* https://stackoverflow.com/questions/70203473/creating-a-horizontal-rule-hr-divider-that-contains-text-with-tailwind-css START */}
+            <div className="relative flex items-center">
+              <div className="flex-grow border-t border-[#111111]"></div>
+              <span className="flex-shrink mx-2 text-[#111111]">or</span>
+              <div className="flex-grow border-t border-[#111111]"></div>
+            </div>
+            {/* END */}
+            <br />
+            <div>
+              <button className="bg-[#ffffff] text-[#111111] border-2 border-[#dcdcdc] w-full h-10 rounded-lg">
+                <div className="flex">
+                  <img
+                    className="h-6 w-6 ml-9"
+                    src="google_login_logo.png"
+                  ></img>
+                  <span className="ml-6">Sign in with Google</span>
+                </div>
+              </button>
+            </div>
+            <br />
+            <div>
+              <button className="bg-[#ffffff] text-[#111111] border-2 border-[#dcdcdc] w-full h-10 rounded-lg">
+                <div className="flex">
+                  <img
+                    className="h-6 w-6 ml-9"
+                    src="facebook_login_logo.png"
+                  ></img>
+                  <span className="ml-6">Sign in with Facebook</span>
+                </div>
+              </button>
+            </div>
+            <br />
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
